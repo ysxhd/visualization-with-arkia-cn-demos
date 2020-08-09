0b85038ab7ac94ad90517dbb48fad9c4e30aca3c
李冬杰 <lidongjies@gmail.com>
2020-08-09 12:18:07 +0800

    feat: add config

---
+++ util/vector2.js
@@ -0,0 +1,54 @@
+export default class Vector2D extends Array {
+	constructor(x = 1, y = 0) {
+		super(x, y);
+	}
+
+	get x() {
+		return this[0];
+	}
+
+	set x(val) {
+		this[0] = val;
+	}
+
+	get y() {
+		return this[1];
+	}
+
+	set y(val) {
+		this[1] = val;
+	}
+
+	get length() {
+		return Math.hypot(this.x, this.y);
+	}
+
+	get dir() {
+		return Math.atan2(this.y, this.x);
+	}
+
+	copy() {
+		return new Vector2D(this.x, this.y);
+	}
+
+	add(v) {
+		this.x += v.x;
+		this.y += v.y;
+		return this;
+	}
+
+	scale(a) {
+		this.x = this.x * a;
+		this.y = this.y * a;
+		return this;
+	}
+
+	rotate(rad) {
+		const c = Math.cos(rad);
+		const s = Math.sin(rad);
+		const [x, y] = this;
+		this.x = x * c + y * -s;
+		this.y = x * s + y * c;
+		return this;
+	}
+}
