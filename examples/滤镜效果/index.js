99ec150798b64cdf51efd94b92a937e26eb2ddf4
李冬杰 <lidongjies@gmail.com>
2020-08-30 18:01:15 +0800

    feat: sync code

--- examples/滤镜效果/index.js
+++ examples/滤镜效果/index.js
@@ -7,11 +7,11 @@ const context = canvas.getContext('2d');
 (async function () {
 	const img = await loadImage('https://p2.ssl.qhimg.com/d/inn/4b7e384c55dc/girl1.jpg');
 	const imageData = getImageData(img);
-	traverse(imageData, ({ r, g, b, a }) => {
-		return transformColor([r, g, b, a], channel({ r: 1.2 }), brightness(1.2), saturate(1.2));
-	});
-	// const { data, width, height } = imageData;
-	// gaussianBlur(data, width, height);
+	// traverse(imageData, ({ r, g, b, a }) => {
+	// 	return transformColor([r, g, b, a], channel({ r: 1.2 }), brightness(1.2), saturate(1.2));
+	// });
+	const { data, width, height } = imageData;
+	gaussianBlur(data, width, height);
 	canvas.width = imageData.width;
 	canvas.height = imageData.height;
 	context.putImageData(imageData, 0, 0);
